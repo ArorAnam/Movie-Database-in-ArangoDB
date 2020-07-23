f9e928f2d90168c783c96a7fef4b459d4adf3713
Naman Arora <naarora@tcd.ie>
2020-07-23 18:28:05 +0530

    Implemented traversal

--- insert.py
+++ insert.py
@@ -24,7 +24,8 @@ is_hero = Movies.edge_collection('is_hero')
 is_heroine = Movies.edge_collection('is_heroine')
 is_villain = Movies.edge_collection('is_villain')
 
-# insert film-1
+# FILM #1
+# --------------------------------------------------------------------------
 films.insert({'_key': 'TheMatrix', 'title': 'The Matrix', 'released': 1999})
 
 actors.insert({'_key': 'Keanu', 'name': 'Keanu Reaves', 'born': 1964})
@@ -85,3 +86,67 @@ is_villain.insert({
     '_from': 'actors/Hugo',
     '_to': 'villain/matrix-villain'
 })
+
+
+# # FILM #2
+# # --------------------------------------------------------------------------
+# films.insert({'_key': 'TheMatrixReloaded', 'title': 'The Matrix Reloaded', 'released': 2003})
+#
+# actors.insert({'_key': 'Keanu', 'name': 'Keanu Reaves', 'born': 1964})
+# actors.insert({'_key': 'Carrie', 'name': 'Carrie-Anne Moss', 'born': 1967})
+# actors.insert({'_key': "Hugo", 'name': 'Hugo Weaving', 'born': 1960})
+#
+# directors.insert({'_key': 'Lana', 'name': 'Lana Wachowski', 'born': 1965})
+# directors.insert({'_key': 'Lily', 'name': 'Lily Wachowski', 'born': 1967})
+#
+# hero.insert({'_key': 'matrix-hero', 'role': 'Neo'})
+# heroine.insert({'_key': 'matrix-heroine', 'role': 'Trinity'})
+# villain.insert({'_key': 'matrix-villain', 'role': 'Agent Smith'})
+#
+# acts_in.insert({
+#     '_key': 'matrix-keanu',
+#     '_from': 'actors/Keanu',
+#     '_to': 'films/TheMatrix'
+# })
+#
+# acts_in.insert({
+#     '_key': 'matrix-carrie',
+#     '_from': 'actors/Carrie',
+#     '_to': 'films/TheMatrix'
+# })
+#
+# acts_in.insert({
+#     '_key': 'matrix-hugo',
+#     '_from': 'actors/Hugo',
+#     '_to': 'films/TheMatrix'
+# })
+#
+# directs.insert({
+#     '_key': 'matrix-lana',
+#     '_from': 'directors/Lana',
+#     '_to': 'films/TheMatrix'
+# })
+#
+# directs.insert({
+#     '_key': 'matrix-lily',
+#     '_from': 'directors/Lily',
+#     '_to': 'films/TheMatrix'
+# })
+#
+# is_hero.insert({
+#     '_key': 'matrix-neo',
+#     '_from': 'actors/Keanu',
+#     '_to': 'hero/matrix-hero'
+# })
+#
+# is_heroine.insert({
+#     '_key': 'matrix-trinity',
+#     '_from': 'actors/Carrie',
+#     '_to': 'heroine/matrix-heroine'
+# })
+#
+# is_villain.insert({
+#     '_key': 'matrix-smith',
+#     '_from': 'actors/Hugo',
+#     '_to': 'villain/matrix-villain'
+# })
---
+++ traverse.py
@@ -0,0 +1,43 @@
+from arango import ArangoClient
+
+# Initialize the ArangoDB client.
+client = ArangoClient()
+
+# Connect to "test" database as root user.
+db = client.db('movies', username='root', password='naman02')
+
+# graph
+Movies = db.graph('Movies')
+
+# # vertices
+# films = Movies.vertex_collection('films')
+# actors = Movies.vertex_collection('actors')
+# directors = Movies.vertex_collection('directors')
+# hero = Movies.vertex_collection('hero')
+# heroine = Movies.vertex_collection('heroine')
+# villain = Movies.vertex_collection('villain')
+#
+# # edges
+# acts_in = Movies.edge_collection('acts_in')
+# directs = Movies.edge_collection('directs')
+# is_hero = Movies.edge_collection('is_hero')
+# is_heroine = Movies.edge_collection('is_heroine')
+# is_villain = Movies.edge_collection('is_villain')
+
+# traverse from hero outbound
+trv = Movies.traverse(
+    start_vertex='actors/Keanu',
+    direction='outbound',
+    strategy='bfs',
+    edge_uniqueness='global',
+    vertex_uniqueness='global'
+)
+
+vertices = trv['vertices']
+
+for i in range(1, len(vertices)):
+    if 'film' in vertices[i]['_id']:
+        print(vertices[i]['title'])
+
+
+
